2a46622695000f66e88ac9c6a6b0fabaa95dbe20
trunghoang2102 <trunghoang21022003@gmail.com>
2021-08-12 20:13:26 +0700

    modifi 2 files

--- Phương trình bậc 2/ptb2.js
+++ Phương trình bậc 2/ptb2.js
@@ -6,21 +6,21 @@ function giai_phuong_trinh_bac_hai() {
     if(a == 0) {
         if(b == 0) {
             if(c == 0) {
-                document.getElementById("result").innerHTML = "Vo so nghiem";
+                document.getElementById("result").innerHTML = "Phương trình có vô số nghiệm";
             }else {
-                document.getElementById("result").innerHTML = "Vo nghiem";
+                document.getElementById("result").innerHTML = "Phương trình vô nghiệm";
             }
         }else {
-            document.getElementById("result").innerHTML = "Nghiem x =" + -c/b;
+            document.getElementById("result").innerHTML = "Phương trình có nghiệm x =" + -c/b;
         }
     }else {
         var d = Math.pow(b, 2) - 4*a*c;
         if(d < 0) {
-            document.getElementById("result").innerHTML = "Vo nghiem";
+            document.getElementById("result").innerHTML = "Phương trình vô nghiệm";
         }else if(d == 0) {
-            document.getElementById("result").innerHTML = "Nghiem kep x1 = x2 = " + -b/2*a
+            document.getElementById("result").innerHTML = "Phương trình có nghiệm kép x1 = x2 = " + -b/2*a
         }else {
-            document.getElementById("result").innerHTML = "x1 = " + (-b + Math.sqrt(d)) / (2*a) + "," + "x2 = " + (-b - Math.sqrt(d)) / (2*a)
+            document.getElementById("result").innerHTML = "Phương trình có 2 nghiệm phân biệt x1 = " + (-b + Math.sqrt(d)) / (2*a) + "và x2 = " + (-b - Math.sqrt(d)) / (2*a)
         }
     }
 }
