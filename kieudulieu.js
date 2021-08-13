736fccd6711558c4bb87af1b98cb3b5ce36c14de
trunghoang2102 <trunghoang21022003@gmail.com>
2021-08-13 21:15:44 +0700

    add 2 files

---
+++ kieudulieu.js
@@ -0,0 +1,16 @@
+let x = 5
+if(x ++ > 5) {
+    console.log('xin chao')
+} else {
+    console.log('Viet Nam')
+}
+
+/*
+++a: tien to
+a++: hau to
+
+-Giong nhau: a cung tang len 1 don vi
+-Khac nhau: Trinh tu
+    _ ++a: tang len truoc roi moi thuc hien phep toan
+    _ a++: thuc hien phep toan roi moi tang len truoc 
+*/
