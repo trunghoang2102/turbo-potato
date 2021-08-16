e9d80de621b2855579564b6b82fe77234fc972aa
trunghoang2102 <trunghoang21022003@gmail.com>
2021-08-16 10:21:01 +0700

    add 3 files

---
+++ Chương trình in nội dung bức thư/my.js
@@ -0,0 +1,7 @@
+function print_document() {
+let a = document.getElementById('name').value;
+let b = document.getElementById('place').value;
+let c = document.getElementById('time').value;
+
+document.getElementById('result').innerHTML = a + " thương nhớ,\nEm không biết phải nói sao để anh hiểu rằng, em nhớ anh thật nhiều. Em yêu anh đến khi trái tim\nnày tan thành nghìn mảnh. Tất cả những gì em yêu thương, em khát khao và cần đến, chính là\nanh, mãi mãi về sau. Em chỉ muốn ở bên anh và yêu anh hỡi, em sẽ trở thành người phụ nữ như\nanh mong muốn.\nCó phải em thật tệ hại, khi cứ nghĩ đến anh thật nhiều, thật lâu và nhất là mỗi khi đêm xuống?\nEm hứa sẽ cố gắng triệu triệu lần hơn thế. Nhưng hơn tất cả, em chỉ mong một ngày nào đó,\nanh sẽ tự hào về em, như tự hào về vợ của anh, và mẹ của các con anh (ít nhất là hai nhé, em vừa\nmới quyết định đấy!). Em nhớ thật nhiều cảm giác mỗi đêm anh ôm em và ru em ngủ trong vòng\ntay. Đêm nay, em chỉ muốn được gần bên anh...và anh biết không, trái tim em đang đau đớn đế\nnhường nào.\nAnh yêu thương, đừng bao giờ rời xa em nữa nhé. Yêu anh rất nhiều.\n" + b + c
+}
