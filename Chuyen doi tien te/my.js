a4420ef7a8974baf5768b9930205fda3b80a21cb
trunghoang2102 <trunghoang21022003@gmail.com>
2021-08-18 08:11:49 +0700

    modify 2 files

--- Chuyen doi tien te/my.js
+++ Chuyen doi tien te/my.js
@@ -1,7 +1,7 @@
 function chuyentien() {
     let a = +document.getElementById('from_currency').value;
     let b = +document.getElementById('to_currency').value;
-    let c = +document.getElementById('sotien').value;
+    let c = +document.getElementById('sotien').value;  
     let d = c * b / a;
     document.getElementById('result').innerHTML = "Số tiền sau khi chuyển đổi là " + d 
 }
