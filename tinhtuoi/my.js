f6c1672d326b473cbc610afefbae1bd725c54337
trunghoang2102 <trunghoang21022003@gmail.com>
2021-08-19 16:55:32 +0700

    .

---
+++ tinhtuoi/my.js
@@ -0,0 +1,16 @@
+function tinh_tuoi() {
+    let birthday = document.getElementById('birthday').value;
+    let date = new Date(birthday);
+    let birthday_year = date.getFullYear();
+
+    let today = new Date();
+    let today_year = today.getFullYear();
+
+    if(date.getMonth() > today.getMonth()){
+        var tuoi = today_year - birthday_year - 1;
+    }else{
+        var tuoi = today_year - birthday_year;
+    }
+    
+    document.getElementById('result').innerHTML = "Tuoi cua ban la " + tuoi;
+}
