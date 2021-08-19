6a5bdc8f4bc7c85a15d1cf421267cf3df7bce159
trunghoang2102 <trunghoang21022003@gmail.com>
2021-08-19 13:34:45 +0700

    delete 2 files and modify 1 file

--- kieudulieu.js
+++ kieudulieu.js
@@ -1,5 +1,5 @@
 let x = 5
-if(x ++ > 5) {
+if (x++ > 5) {
     console.log('xin chao')
 } else {
     console.log('Viet Nam')
@@ -12,5 +12,17 @@ a++: hau to
 -Giong nhau: a cung tang len 1 don vi
 -Khac nhau: Trinh tu
     _ ++a: tang len truoc roi moi thuc hien phep toan
-    _ a++: thuc hien phep toan roi moi tang len truoc 
+    _ a++: thuc hien phep toan roi moi tang len truoc
+*/
+
+/*
+    + chuong trinh = input + thuat toan + output
+    + cach nhan input:
+        - prompt() (it dung)
+        - lay thong tin tu phan tu html -> document.getElementById('......')
+    + Xuat du lieu
+        - console.log() -> hien thi du lieu o cua so console (thuong dung trong debug)
+        - alert() -> xuat hien dang pop-up o ngoai man hinh
+        - document.write() (it dung)
+        - .innerHTML: Thay doi noi dung phan tu HTML
 */
