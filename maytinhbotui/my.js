57f30008a273edfdde102274409bf8763adebc19
trunghoang2102 <trunghoang21022003@gmail.com>
2021-08-20 15:12:49 +0700

    modify 1 file

--- maytinhbotui/my.js
+++ maytinhbotui/my.js
@@ -7,7 +7,7 @@ function tinh() {
     var b = eval(a);
     
     if (b === Infinity){
-        document.getElementById("display").value = "Math ERROR";
+        document.getElementById("display").value = "Math giERROR";
         return;
     }
 
